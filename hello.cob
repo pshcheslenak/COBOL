ac324e1e6f5d480d4db5a41c436a69932c4a9f66
Petr Shcheslenak <pshcheslenok@gmail.com>
2021-06-21 21:35:03 +0300

    First cobol program.

---
+++ hello.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+           PROGRAM-ID. HELLO.
+           AUTHOR. PETR.
+           INSTALLATION. MyComputer.
+           DATE-WRITTEN. 21/06/2021.
+           DATE-COMPILED. 21/06/2021.
+           SECURITY. No.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+       BEGIN.
+           DISPLAY "Hello World!".
+           STOP RUN.
